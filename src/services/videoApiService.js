26a69fffd8c916cd59080c3bcf2a0d1301d48e8f
Carlos <carlos.bejarano.2017@upb.edu.co>
2021-03-21 09:45:41 -0700

    Agregando instancias axios

---
+++ src/services/videoApiService.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+import { getLocalSesion } from '../util/auth'
+
+const getAxiosInstance = () => {
+    return axios.create({
+        baseURL: `${process.env.REACT_APP_GATEWAY_SERVICE_BASE_URL}/api`
+    });
+}
+
+export const listVideos = async () => {
+    const { token } = await getLocalSesion();
+    const axiosInstance = await getAxiosInstance()
+    return axiosInstance.get(`/video`,
+        { headers: { 'Authorization': `Bearer ${token}`, 'Content-Type': 'application/json' } })
+}
+
+export const download = async (videoId) => {
+    const { token } = await getLocalSesion();
+    const axiosInstance = await getAxiosInstance()
+    return axiosInstance.get(`/photo/download/${videoId}`,
+        {
+            headers: { 'Authorization': `Bearer ${token}` },
+            responseType: 'blob'
+        })
+}
+
+export const removeVideos = async (video, isVideo = true) => {
+    const { token } = await getLocalSesion();
+    const axiosInstance = await getAxiosInstance()
+    return axiosInstance.delete(`${isVideo ? '/video' : '/photo'}`,
+        {
+            data: { files: video },
+            headers: { 'Authorization': `Bearer ${token}`, 'Content-Type': 'application/json' }
+        })
+}
